c6c95a467f8e669192f7eebf5561f3278cf2c332
Vinícius Santos <vini.santos.valentim@gmail.com>
2026-03-17 14:41:53 -0500

    Add files via upload

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -11,6 +11,7 @@ import javax.swing.*;
 public class Hello2DApp {
     public static void main (String[] args) {
         Hello2DFrame frame = new Hello2DFrame();
+    
     }
 }
 
@@ -26,30 +27,38 @@ class Hello2DFrame extends JFrame {
         this.setTitle("Java2D - Hello World!");
         this.setSize(350, 600);
         this.setVisible(true);
+    	this.setBackground(Color.green);
     }
 
     public void paint (Graphics g) {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D) g; 
+        Graphics2D g2d = (Graphics2D) g; 	
+	g2d.setBackground(Color.green);
+	super.paint(g);
+	this.setBackground(Color.green);
 	int w = getWidth();
         int h = getHeight();
- 	
-	Shape losango = Shape(0,0, w/2,h/2, 0,0);
 
-       	//g2d.setPaint(Color.red);
-	//g2d.fillRect(0,0, w,h);
-	g2d.setBackground(Color.red);
+	// setando background
+       	g2d.setPaint(Color.green);
+	g2d.fillRect(0,0, w,h);
+
+	int pointxs[] = {w/2, w-20, w/2, 20};
+	int pointys[] = {40, h/2, h - 10, h/2};
+	
+	// desenhando um polígono um pouco mais complexo
+       	g2d.setPaint(Color.yellow);	
+	g2d.fillPolygon(pointxs, pointys, pointxs.length);
+	
+	g2d.setPaint(Color.blue);
+	//g2d.fillOval(w/2-175, h/2-120, 350, 240);
+	g2d.fillOval(w/4, h/4, w/2, h/2);
 
 	g2d.setPaint(Color.blue);
-	
-	g2d.drawOval(w/2,h/2, w,h);
-        g2d.drawLine(0,h, w,0);
     	
-	g2d.setPaint(Color.green);
-	g2d.drawShape(losango);
+	//g2d.setPaint(Color.green);
+	//g2d.drawShape(losango);
     }
 }
-
 
 
 
