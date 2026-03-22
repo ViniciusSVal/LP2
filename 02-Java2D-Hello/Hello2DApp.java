730c049ab547f483960c4398defbd9c237a262ab
Vinícius Santos <vini.santos.valentim@gmail.com>
2026-03-22 01:32:34 -0300

    Update Hello2DApp.java

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -29,34 +29,34 @@ class Hello2DFrame extends JFrame {
     }
 
     public void paint (Graphics g) {
-	//limpando a tela
-	super.paint(g);
-
-	// iniciando o contexto grafico e variaveis auxiliares 
-	Graphics2D g2d = (Graphics2D) g; 	
-	int w = getWidth();
-    int h = getHeight();
-	int SPACE = 20; 	// espaço das bordas para o losango
-	final int BAR = 25; 	// altura da barra superior do aplicativo (pelo menos na minha maquina)
-
-	// a) setando o background
-	g2d.setPaint(Color.green);
-	g2d.fillRect(0,0, w,h); // aqui e necessario a barra estar contando na altura, pois se nao ele nao preenche a tela inteira
-
-	// desconsiderando a barra 
-	h -= BAR;
+		//limpando a tela
+		super.paint(g);
 	
-	// c) desenhando um poligono um pouco mais complexo
-	int pointxs[] = {w/2, 			w -SPACE, 	w/2,			0 +SPACE};
-	int pointys[] = {0 +BAR+SPACE,	h/2 +BAR, 	h +BAR-SPACE, 	h/2 +BAR};
-
-	g2d.setPaint(Color.yellow);	
-	g2d.fillPolygon(pointxs, pointys, pointxs.length);
-
-	// b) desenhando primitivas graficas
-	SPACE /= 2;
-	g2d.setPaint(Color.blue);
-	g2d.fillOval(w/4 +SPACE, h/4 +BAR+SPACE,   (3*w)/4 -(w/4) -2*SPACE, 3*h/4 -(h/4) -2*SPACE);
+		// iniciando o contexto grafico e variaveis auxiliares 
+		Graphics2D g2d = (Graphics2D) g; 	
+		int w = getWidth();
+	    int h = getHeight();
+		int SPACE = 20; 		// espaço das bordas para o losango
+		final int BAR = 25; 	// altura da barra superior do aplicativo (pelo menos na minha maquina)
+	
+		// a) setando o background
+		g2d.setPaint(Color.green);
+		g2d.fillRect(0,0, w,h); // aqui e necessario a barra estar contando na altura, pois se nao ele nao preenche a tela inteira
+	
+		// desconsiderando a barra 
+		h -= BAR;
+		
+		// c) desenhando um poligono um pouco mais complexo
+		int pointxs[] = {w/2, 			w -SPACE, 	w/2,			0 +SPACE};
+		int pointys[] = {0 +BAR+SPACE,	h/2 +BAR, 	h +BAR-SPACE, 	h/2 +BAR};
+	
+		g2d.setPaint(Color.yellow);	
+		g2d.fillPolygon(pointxs, pointys, pointxs.length);
+	
+		// b) desenhando primitivas graficas
+		SPACE /= 2;
+		g2d.setPaint(Color.blue);
+		g2d.fillOval(w/4 +SPACE, h/4 +BAR+SPACE,   (3*w)/4 -(w/4) -2*SPACE, 3*h/4 -(h/4) -2*SPACE);
     }
 }
 
