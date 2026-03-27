9a97daab6c0d3082024154febf87043e84962205
Vinícius Santos <vini.santos.valentim@gmail.com>
2026-03-27 16:04:55 -0300

    (2.1.3) ajeitando tabs

--- Classroom/2.1.3/RectAPP.java
+++ Classroom/2.1.3/RectAPP.java
@@ -1,42 +1,42 @@
 public class RectAPP {
-        public static void main (String[] args) {
-                Rect r1 = new Rect(0, 0, 10, 10);
-                r1.print();
-
-                System.out.println("Movendo + (5, 5)");
-                r1.drag(5, 5);
-
-               	r1.print();
-
-                System.out.println("Movendo + (-10, 100)");
-                r1.drag(-10, 100);
-
-               	r1.print();
-        }
+	public static void main (String[] args) {
+		Rect r1 = new Rect(0, 0, 10, 10);
+		r1.print();
+		
+		System.out.println("Movendo + (5, 5)");
+		r1.drag(5, 5);
+		
+		r1.print();
+		
+		System.out.println("Movendo + (-10, 100)");
+		r1.drag(-10, 100);
+		
+		r1.print();
+    }
 }
 
 class Rect {
 	int x, y; // pos
-        int w, h; // dim
+	int w, h; // dim
 
-       	Rect (int x, int y, int w, int h) {
-                this.x = x;
-                this.y = y;
-                this.w = w;
-                this.h = h;
-        }
+	Rect (int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
 
 	int area () {
-                return this.w * this.h;
-        }
+		return this.w * this.h;
+	}
 
 	void drag (int dx, int dy) {
-                this.x += dx;
-                this.y += dy;
-        }
+		this.x += dx;
+		this.y += dy;
+	}
 
 	void print () {
-                System.out.printf("Retangulo de dimensoes W = %d e H = %d com area %d na posiçao (%d, %d)\n", 
-                                this.w, this.h, this.area(), this.x, this.y);
-        }
+		System.out.printf("Retangulo de dimensoes W = %d e H = %d com area %d na posiçao (%d, %d)\n", 
+			this.w, this.h, this.area(), this.x, this.y);
+	}
 }
